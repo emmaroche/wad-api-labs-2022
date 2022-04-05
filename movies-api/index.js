74d31a586c554dbd350526510cf8210a8a46c063
Emma Roche <emmaroche@Emmas-MBP.wit.ie>
2022-04-05 10:06:58 +0100

    Week 10 Lab A exercise done

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 
 dotenv.config();
 
@@ -8,10 +9,13 @@ const app = express();
 
 const port = process.env.PORT;
 
+app.use(express.json());
 app.use('/api/movies', moviesRouter);
+
+app.use('/api/genres', genresRouter);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
 
-app.use(express.json());
