14d8739504b31ccb59409cc5faaffd7f086a4f78
Emma Roche <emmaroche@Emmas-MBP.wit.ie>
2022-04-26 10:06:20 +0100

    Topic 10 - Update Movies Data/Route

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -3,6 +3,7 @@ import User from './userModel';
 import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
 
+
 const router = express.Router(); // eslint-disable-line
 
 // Get all users
